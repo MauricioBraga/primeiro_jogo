024e0c8ca47b7f2ecc695cd16ebf23990bc79e72
Maurício da Motta Braga <braga.mauricio@gmail.com>
2024-09-26 20:30:09 -0300

    Initial commit

---
+++ Assets/scripts/TelaCreditosGameState.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class TelaCreditosGameState : GameBaseState
+{
+    private GameObject telaCreditosJogo;
+    public override void enterState(GameStateManager gameState)
+    {
+        Debug.Log("Entramos na tela de créditos.");
+        gameState.mensagem.text = "tela créditos";
+        // pega o game object com a tela de creditos, para poder ativar / desativar
+        // a sua aparição (via SpriteRenderer) ao entrar / sair estado.
+        telaCreditosJogo = GameObject.Find("tela_creditos_Treasure_Hunter_1280_1060");
+
+        // ativa o sprite render do gameObject da tela de título, exibindo-a na tela.
+        telaCreditosJogo.GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    public override void updateState(GameStateManager gameState)   {
+    
+ 
+  if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+  {
+        // muda para o próximo estado.
+        gameState.switchState(gameState.introState);
+  }
+}
+
+    public override void leaveState(GameStateManager gameState)
+    {
+        Debug.Log("Saindo da tela de créditos.");
+        // ativa o sprite render do gameObject da tela de título, exibindo-a na tela.
+        telaCreditosJogo.GetComponent<SpriteRenderer>().enabled = false;
+    }
+}
+    
---
+++ Assets/scripts/TelaInicialGameState.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class TelaInicialGameState : GameBaseState
+{
+    private GameObject telaInicialJogo;
+    
+
+    public override void enterState(GameStateManager gameState)
+    {
+        Debug.Log("Entramos na Tela inicial.");
+        gameState.mensagem.text = "tela inicial";
+        // pega o game object com a tela de título, para poder ativar / desativar
+        // a sua aparição (via SpriteRenderer) ao entrar / sair estado.
+        telaInicialJogo = GameObject.Find("Tela_treasure_hunter_1280_x_1060");
+
+        // ativa o sprite render do gameObject da tela de título, exibindo-a na tela.
+        telaInicialJogo.GetComponent<SpriteRenderer>().enabled = true;
+
+    }
+
+    public override void updateState(GameStateManager gameState)   {
+    
+ 
+  if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+  {
+        // muda para o próximo estado.
+        gameState.switchState(gameState.telaCreditosState);
+  }
+}
+
+    public override void leaveState(GameStateManager gameState)
+    {
+        Debug.Log("Saindo da Tela inicial.");
+
+        // ativa o sprite render do gameObject da tela de título, exibindo-a na tela.
+        telaInicialJogo.GetComponent<SpriteRenderer>().enabled = false;
+    }
+}
